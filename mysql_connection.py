a80f995353a365c51c3025bf0c5a26a944ccfb7b
uaibol <uaibol@gmail.com>
2022-04-09 21:28:29 +0600

    modified

--- mysql_connection.py
+++ mysql_connection.py
@@ -2,7 +2,12 @@ from mysql.connector import connect, Error
 
 try:
     with connect(host="localhost", user="root", password="pass123", database="db_name") as connection:
-        print(connection)
+        if connection.is_connected():
+            print(connection.get_server_info())
+            cursor = connection.cursor()
+            cursor.execute("select database();")
+            db = cursor.fetchone()
+            print("You are connected to the database: ", db)
 except Error as e:
     print(e)
 
