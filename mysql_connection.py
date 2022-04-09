5db400e8450f18a2f1d4a4c054bc5efdc96cbed5
uaibol <uaibol@gmail.com>
2022-04-09 21:22:01 +0600

    modified

--- mysql_connection.py
+++ mysql_connection.py
@@ -1,7 +1,7 @@
 from mysql.connector import connect, Error
 
 try:
-    with connect(host="localhost", user="root", password="pass123") as connection:
+    with connect(host="localhost", user="root", password="pass123", database="db_name") as connection:
         print(connection)
 except Error as e:
     print(e)
